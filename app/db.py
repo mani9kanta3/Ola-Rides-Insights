938d556ae67c626b165e37ba0635094905f369db
PUDI MANIKANTA <151725075+mani9kanta3@users.noreply.github.com>
2026-01-08 13:33:04 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -2,13 +2,9 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-def get_engine():
-    db_url = os.environ.get("DATABASE_URL")
-    if not db_url:
-        raise ValueError("DATABASE_URL environment variable not set")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-    return create_engine(db_url)
+engine = create_engine(DATABASE_URL)
 
 def run_query(query):
-    engine = get_engine()
     return pd.read_sql(query, engine)
